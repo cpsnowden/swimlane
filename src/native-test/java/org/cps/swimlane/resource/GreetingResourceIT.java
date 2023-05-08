b4a63ec68693b672fb2559f598d68ec3f1fe6482
Chris Snowden <chrissnowden35@gmail.com>
2023-05-08 18:52:09 +0100

    Init Quarkus Project

---
+++ src/native-test/java/org/cps/swimlane/resource/GreetingResourceIT.java
@@ -0,0 +1,9 @@
+package org.cps.swimlane.resource;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+import org.cps.swimlane.resource.GreetingResourceTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
